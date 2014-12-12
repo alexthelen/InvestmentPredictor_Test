cd1a5ab0b33eb2a07087df2632aa2ed20ac383ff
athelen <athelen@S21799.sogeti.com>
2014-12-11 20:29:17 -0600

    Wrote unit tests for value check and number of results check.

--- src/DataLayer_Test/YahooFinance_Test.java
+++ src/DataLayer_Test/YahooFinance_Test.java
@@ -2,28 +2,67 @@ package DataLayer_Test;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import org.junit.Test;
 
-import InvestmentPredictor.DataLayer.YahooFinance;
+import InvestmentPredictor.DataLayer.YahooQueryLanguage.FinanceHistoricalData;
+import InvestmentPredictor.DataLayer.YahooQueryLanguage.YahooFinance;
 
 public class YahooFinance_Test 
 {
 	@Test
-	public void testRetrieveData() 
+	public void testGetFinanceHistoricalData_Values() 
 	{
+		Iterable<FinanceHistoricalData> results;
+		ArrayList<String> tickers = new ArrayList<String>();
+		tickers.add("QQQ");
+		
+		Calendar dec_2_2014 = new GregorianCalendar(2014, 11, 2);
+		
+		YahooFinance yf = new YahooFinance(dec_2_2014, dec_2_2014);
+		results = yf.GetFinanceHistoricalData(tickers);
+		
+		assertNotNull(results);
+
+		for(FinanceHistoricalData fhd : results)
+		{
+			assertEquals("QQQ", fhd.GetSymbol());
+			assertEquals(dec_2_2014.getTime(), fhd.GetDate().getTime());
+			assertEquals(new BigDecimal("104.92"), fhd.GetOpenPrice());
+			assertEquals(new BigDecimal("105.42"), fhd.GetHighPrice());
+			assertEquals(new BigDecimal("104.70"), fhd.GetLowPrice());
+			assertEquals(new BigDecimal("105.23"), fhd.GetClosePrice());
+			assertEquals(25400600, fhd.GetVolume());
+			assertEquals(new BigDecimal("105.23"), fhd.GetAdjClose());
+			
+		}
+	}
+	
+	@Test
+	public void testGetFinanceHistoricalData_Number() 
+	{
+		Iterable<FinanceHistoricalData> results;
 		ArrayList<String> tickers = new ArrayList<String>();
 		tickers.add("QQQ");
 		tickers.add("GOOG");
 		tickers.add("GM");
 		
 		Calendar dec_1_2014 = new GregorianCalendar(2014, 11, 1);
-		Calendar dec_2_2014 = new GregorianCalendar(2014, 11, 2);
+		Calendar dec_3_2014 = new GregorianCalendar(2014, 11, 3);
 		
-		YahooFinance yf = new YahooFinance(tickers, dec_1_2014, dec_2_2014);
-		yf.RetrieveData();
+		YahooFinance yf = new YahooFinance(dec_1_2014, dec_3_2014);
+		results = yf.GetFinanceHistoricalData(tickers);
+		
+		assertNotNull(results);
+
+		int count = 0;
+		for(FinanceHistoricalData fhd : results)
+			count++;
+		
+		assertEquals(9, count);
 	}
 }
